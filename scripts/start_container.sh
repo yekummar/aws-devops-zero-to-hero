e9ed4d3750daf2be4b66bbfd64b25225e7e0de60
yekummar <62386322+yekummar@users.noreply.github.com>
2023-12-30 18:16:39 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,9 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull yekummar/yfirstawscbd:latestdoc
 
+# Remove the Docker image existing Process
+containerid='sudo docker ps | awk -F " " '{print $1}''
+sudo docker rm -f $containerid
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 yekummar/yfirstawscbd:latestdoc
